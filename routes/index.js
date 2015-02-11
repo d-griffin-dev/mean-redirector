5fee32f87223e9811f178654299289e6e59f13b5
Daniel Griffin <dangriff50@gmail.com>
2015-02-11 15:41:22 -0500

    cleanup, add mongoose

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+
+
+/* GET default page. */
 router.get('/', function(req, res, next) {
     
-  //  res.redirect(301, DATA.defaultUrl); send to root
-
-  res.render('index', { title: 'Express' });
+    //  res.redirect(301, DATA.defaultUrl); send to root
+    /* Activate below for debugging */
+    res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
